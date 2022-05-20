6dd2dc29d4acbc44ff76872c4ccad9aac4a1dd42
seung kyeong <sk00021@naver.com>
2022-05-21 01:40:53 +0900

    BoardController오류 수정

---
+++ ManiMani/src/main/java/com/manimani/web/controller/BoardRestController.java
@@ -0,0 +1,13 @@
+package com.manimani.web.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.manimani.web.vo.BoardVO;
+
+public class BoardRestController {
+
+	// 게시판 추가 OK
+	
+}
